cc40b727df664a4b37e0fd18850ef1db7caa075d
Yuzuki-niwa <b2100994@gl.aiu.ac.jp>
2023-10-14 17:20:09 +0900

    from cm to feet 完了

---
+++ script.js
@@ -0,0 +1,7 @@
+function convertHeight(){
+    const cmInput = document.getElementById('cmInput').value
+    const feet = Math.floor(cmInput*0.0328084);
+    const inches = Math.round((cmInput * 0.0328084 - feet)*12);
+    const result = `${feet} feet ${inches} inches`;
+    document.getElementById("result").textContent = result;
+}
